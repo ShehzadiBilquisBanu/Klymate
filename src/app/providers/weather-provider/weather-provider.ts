70f1b0eb0cd0a92ab961e93ec355cfb5cea4e826
ShehzadiBilquisBanu <shehzadibilquis325@gmail.com>
2018-12-28 10:41:22 +0530

    files updated

--- src/app/providers/weather-provider/weather-provider.ts
+++ src/app/providers/weather-provider/weather-provider.ts
@@ -5,9 +5,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class WeatherProvider {
+//private apikey: any = '0a3f5e52f1e4ff559f44ae1c4aa05dfc';
+//private loc:string = '';
+ private url = 'http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&APPID=0a3f5e52f1e4ff559f44ae1c4aa05dfc';
 
-  private url = 'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=0a3f5e52f1e4ff559f44ae1c4aa05dfc';
+//private url = 'http://api.openweathermap.org/data/2.5/weather?q='+this.loc+'&units=metric&APPID='+this.apikey+'';
 
+  
   constructor(private httpClient: HttpClient) { }
 
   getWeatherDetails() {
